ef443757bd72a8d21ed5b36cf29012f987e3b889
Alex M <alex@mosstech.io>
2022-01-21 12:19:33 +0000

    Move velero to control-plane setup - needs high privilege

--- backup/terraform/datasources.tf
+++
@@ -1,6 +0,0 @@
-data "google_client_config" "current" {}
-
-data "google_container_cluster" "my_cluster" {
-  name     = var.cluster_name
-  location = var.region
-}
--- backup/terraform/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "project_id" {}
-variable "bucket_name" {}
-variable "namespace" {}
-variable "cluster_name" {}
-variable "region" {}

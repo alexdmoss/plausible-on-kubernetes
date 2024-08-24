cda4c1fc49f237ee51f2502751b2c9f2ece72a00
Alex M <alex@alexos.dev>
2024-08-24 17:29:44 +0100

    Refactor CI for new components

---
+++ setup-secrets.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euoE pipefail
+
+if [[ -z "${GCP_PROJECT_ID:-}" ]]; then
+  echo "-> [ERROR] GCP_PROJECT_ID must be set"
+  exit 1
+fi
+
+pushd "$(dirname "${BASH_SOURCE[0]}")/" > /dev/null
+
+plausible_secrets=$(gcloud secrets versions access latest --secret="PLAUSIBLE" --project="${GCP_PROJECT_ID}")
+# shellcheck disable=2046
+export $(echo "${plausible_secrets}" | xargs)
+
+cat plausible-conf.env | \
+  envsubst "\$ADMIN_USER_EMAIL \$ADMIN_USER_NAME \$ADMIN_USER_PWD" | \
+  envsubst "\$BASE_URL \$SECRET_KEY_BASE \$TOTP_VAULT_KEY" | \
+  envsubst "\$POSTGRES_USER \$POSTGRES_PASSWORD \$CLICKHOUSE_USER \$CLICKHOUSE_PASSWORD" | \
+  envsubst " \$SENDGRID_KEY \$GOOGLE_CLIENT_ID \$GOOGLE_CLIENT_SECRET" | \
+  envsubst "\$TWITTER_CONSUMER_KEY \$TWITTER_CONSUMER_SECRET \$TWITTER_ACCESS_TOKEN \$TWITTER_ACCESS_TOKEN_SECRET" \
+  > k8s/base/plausible-conf.env.secret
+
+kustomize build k8s/secret/ | kubectl apply -f -
+
+trap "rm -f k8s/base/plausible-conf.env.secret" EXIT
+
+popd >/dev/null
--- utils/bootstrap-secrets.sh
+++ utils/bootstrap-secrets.sh
@@ -6,25 +6,3 @@ gcloud services enable secretmanager.googleapis.com --project="${GCP_PROJECT_ID}
 
 ### moved all to one secret
 gcloud secrets create PLAUSIBLE --project="${GCP_PROJECT_ID}"
-
-# gcloud secrets create PLAUSIBLE_ADMIN_USER_EMAIL --project="${GCP_PROJECT_ID}"
-# gcloud secrets create PLAUSIBLE_ADMIN_USER_NAME --project="${GCP_PROJECT_ID}"
-# gcloud secrets create PLAUSIBLE_ADMIN_USER_PWD --project="${GCP_PROJECT_ID}"
-# gcloud secrets create PLAUSIBLE_BASE_URL --project="${GCP_PROJECT_ID}"
-# gcloud secrets create PLAUSIBLE_SECRET_KEY_BASE --project="${GCP_PROJECT_ID}"
-
-# gcloud secrets create PLAUSIBLE_POSTGRES_USER --project="${GCP_PROJECT_ID}"
-# gcloud secrets create PLAUSIBLE_POSTGRES_PASSWORD --project="${GCP_PROJECT_ID}"
-
-# gcloud secrets create PLAUSIBLE_CLICKHOUSE_USER --project="${GCP_PROJECT_ID}"
-# gcloud secrets create PLAUSIBLE_CLICKHOUSE_PASSWORD --project="${GCP_PROJECT_ID}"
-
-# gcloud secrets create PLAUSIBLE_GOOGLE_CLIENT_ID --project="${GCP_PROJECT_ID}"
-# gcloud secrets create PLAUSIBLE_GOOGLE_CLIENT_SECRET --project="${GCP_PROJECT_ID}"
-
-# gcloud secrets create PLAUSIBLE_TWITTER_CONSUMER_KEY --project="${GCP_PROJECT_ID}"
-# gcloud secrets create PLAUSIBLE_TWITTER_CONSUMER_SECRET --project="${GCP_PROJECT_ID}"
-# gcloud secrets create PLAUSIBLE_TWITTER_ACCESS_TOKEN --project="${GCP_PROJECT_ID}"
-# gcloud secrets create PLAUSIBLE_TWITTER_ACCESS_TOKEN_SECRET --project="${GCP_PROJECT_ID}"
-
-# gcloud secrets create PLAUSIBLE_SENDGRID_KEY --project="${GCP_PROJECT_ID}"
